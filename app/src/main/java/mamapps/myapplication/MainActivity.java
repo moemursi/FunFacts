1cf557c36b6ba786a0cfe0cd5667eda163a00744
moemursi <jemkarry@outlook.com>
2017-07-21 02:17:47 -0400

    Final commit

--- app/src/main/java/mamapps/myapplication/MainActivity.java
+++ app/src/main/java/mamapps/myapplication/MainActivity.java
@@ -1,15 +1,13 @@
 package mamapps.myapplication;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.security.PrivilegedAction;
-import java.util.Random;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // Declare the view variables
@@ -42,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mshowFactButton.setOnClickListener(listener);
+        Toast.makeText(MainActivity.this, "Welcome To the Fun Facts !!! ", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", "we are logging from oncreate");
 
 
 
